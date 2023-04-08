cf6695c5c966f957669bd94629611b6750803a63
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-08 10:18:54 +0600

    Getting t-shirt from tshirt component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Cart from "../Cart/Cart";
 import Tshirt from "../Tshirt/Tshirt";
@@ -6,11 +6,21 @@ import "./Home.css";
 
 const Home = () => {
   const tshirts = useLoaderData();
+
+  const [cart, setCart] = useState([]);
+  const handleAddToCart = (tshirt) => {
+    console.log(tshirt);
+  };
+
   return (
     <div className="home-container">
       <div className="t-shirt-container">
         {tshirts.map((tshirt) => (
-          <Tshirt tshirt={tshirt} />
+          <Tshirt
+            key={tshirt._id}
+            tshirt={tshirt}
+            handleAddToCart={handleAddToCart}
+          />
         ))}
       </div>
       <div className="cart-container">
--- src/components/Tshirt/Tshirt.jsx
+++ src/components/Tshirt/Tshirt.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Tshirt.css";
 
-const Tshirt = ({ tshirt }) => {
+const Tshirt = ({ tshirt, handleAddToCart }) => {
   const { picture, name, price } = tshirt;
   return (
     <div className="t-shirt">
       <img src={picture} alt="" />
       <h4>{name}</h4>
       <p>Price: ${price}</p>
-      <button>Buy Now</button>
+      <button onClick={() => handleAddToCart(tshirt)}>Buy Now</button>
     </div>
   );
 };
